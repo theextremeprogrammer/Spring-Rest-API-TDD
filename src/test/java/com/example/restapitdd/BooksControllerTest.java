5e968c93f8605701688f337042a24ef187630714
Derek Lee <dereklee@downtothis.com>
2019-09-23 14:19:46 +0900

    Refactors tests into setup method

--- src/test/java/com/example/restapitdd/BooksControllerTest.java
+++ src/test/java/com/example/restapitdd/BooksControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.restapitdd;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -10,24 +11,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class BooksControllerTest {
-    @Test
-    public void test_getBooksController_returnsOkHttpStatus() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders
                 .standaloneSetup(new BooksController())
                 .build();
+    }
 
-
+    @Test
+    public void test_getBooksController_returnsOkHttpStatus() throws Exception {
         mockMvc.perform(get("/api/books/hardcoded"))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void test_getBooksController_returnsAnEmptyList() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders
-                .standaloneSetup(new BooksController())
-                .build();
-
-
         mockMvc.perform(get("/api/books/hardcoded"))
                 .andExpect(jsonPath("$", hasSize(0)));
     }
