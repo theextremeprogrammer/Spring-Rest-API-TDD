a6377f7735b97807cf561df7bded78569223d30f
Derek Lee <dereklee@downtothis.com>
2019-09-23 14:18:38 +0900

    Second passing test for empty list from get endpoint

--- src/main/java/com/example/restapitdd/BooksController.java
+++ src/main/java/com/example/restapitdd/BooksController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/books")
 public class BooksController {
     @GetMapping("/hardcoded")
-    public void getBooksHardcoded() {
-
+    public List<String> getBooksHardcoded() {
+        return new ArrayList<>();
     }
 }
--- src/test/java/com/example/restapitdd/BooksControllerTest.java
+++ src/test/java/com/example/restapitdd/BooksControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class BooksControllerTest {
@@ -18,4 +20,17 @@ public class BooksControllerTest {
         mockMvc.perform(get("/api/books/hardcoded"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void test_getBooksController_returnsAnEmptyList() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders
+                .standaloneSetup(new BooksController())
+                .build();
+
+
+        mockMvc.perform(get("/api/books/hardcoded"))
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+
 }
