e8f79f89ab8452ad44a71d9c03de602f5a7b18da
Derek Lee <derek@sunrisingappdev.com>
2021-04-06 22:18:57 +0900

    Converts FakeBooksRepositoryTest to a contract test using the Template OO design pattern
    - Then, the FakeBooksRepository and any other repository that conforms to BooksRepository can be run against this contract to ensure consistent behavior.

---
+++ src/test/java/com/example/restapitdd/BooksRepositoryContractTestTemplate.java
@@ -0,0 +1,59 @@
+package com.example.restapitdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+public abstract class BooksRepositoryContractTestTemplate<BooksRepositoryType extends BooksRepository> {
+    protected BooksRepositoryType booksRepository;
+
+    protected abstract BooksRepositoryType createBooksRepository();
+
+    @Before
+    final public void setUp() {
+        booksRepository = createBooksRepository();
+    }
+
+    @Test
+    final public void fakeBooksRepo_whenNoBooksHaveBeenSaved_hasNoBooks() {
+        assertThat(booksRepository.getAll().size(), equalTo(0));
+    }
+
+    @Test
+    final public void fakeBooksRepo_whenBooksHaveBeenSaved_hasBooks() {
+        booksRepository.create(new NewBook("", ""));
+
+
+        assertThat(booksRepository.getAll().size(), greaterThan(0));
+    }
+
+    @Test
+    final public void fakeBooksRepo_whenBooksHaveBeenSaved_returnsAllBooks() {
+        booksRepository.create(new NewBook("TDD by Example", "Kent Beck"));
+        booksRepository.create(new NewBook("Refactoring", "Martin Fowler"));
+
+
+        List<Book> allBooks = booksRepository.getAll();
+
+
+        assertThat(allBooks.get(0), equalTo(new Book(1, "TDD by Example", "Kent Beck")));
+        assertThat(allBooks.get(1), equalTo(new Book(2, "Refactoring", "Martin Fowler")));
+    }
+
+    @Test
+    final public void fakeBooksRepo_whenBooksHaveBeenSaved_returnsBookById() {
+        booksRepository.create(new NewBook("TDD by Example", "Kent Beck"));
+        booksRepository.create(new NewBook("Refactoring", "Martin Fowler"));
+
+
+        Book actualBook = booksRepository.getById(2);
+
+
+        assertThat(actualBook, equalTo(new Book(2, "Refactoring", "Martin Fowler")));
+    }
+}
--- src/test/java/com/example/restapitdd/FakeBooksRepositoryTest.java
+++ src/test/java/com/example/restapitdd/FakeBooksRepositoryTest.java
@@ -1,57 +1,8 @@
 package com.example.restapitdd;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-
-public class FakeBooksRepositoryTest {
-    private FakeBooksRepository fakeBooksRepository;
-
-    @Before
-    public void setUp() {
-        fakeBooksRepository = new FakeBooksRepository();
-    }
-
-    @Test
-    public void fakeBooksRepo_whenNoBooksHaveBeenSaved_hasNoBooks() {
-        assertThat(fakeBooksRepository.getAll().size(), equalTo(0));
-    }
-
-    @Test
-    public void fakeBooksRepo_whenBooksHaveBeenSaved_hasBooks() {
-        fakeBooksRepository.create(new NewBook("", ""));
-
-
-        assertThat(fakeBooksRepository.getAll().size(), greaterThan(0));
-    }
-
-    @Test
-    public void fakeBooksRepo_whenBooksHaveBeenSaved_returnsAllBooks() {
-        fakeBooksRepository.create(new NewBook("TDD by Example", "Kent Beck"));
-        fakeBooksRepository.create(new NewBook("Refactoring", "Martin Fowler"));
-
-
-        List<Book> allBooks = fakeBooksRepository.getAll();
-
-
-        assertThat(allBooks.get(0), equalTo(new Book(1, "TDD by Example", "Kent Beck")));
-        assertThat(allBooks.get(1), equalTo(new Book(2, "Refactoring", "Martin Fowler")));
-    }
-
-    @Test
-    public void fakeBooksRepo_whenBooksHaveBeenSaved_returnsBookById() {
-        fakeBooksRepository.create(new NewBook("TDD by Example", "Kent Beck"));
-        fakeBooksRepository.create(new NewBook("Refactoring", "Martin Fowler"));
-
-
-        Book actualBook = fakeBooksRepository.getById(2);
-
-
-        assertThat(actualBook, equalTo(new Book(2, "Refactoring", "Martin Fowler")));
+public class FakeBooksRepositoryTest extends BooksRepositoryContractTestTemplate<FakeBooksRepository> {
+    @Override
+    protected FakeBooksRepository createBooksRepository() {
+        return new FakeBooksRepository();
     }
 }
